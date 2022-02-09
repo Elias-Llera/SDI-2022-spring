2a18544b16fca6086050f9f7cfb695a3b57352eb
Elias <eliaslleragr@gmail.com>
2022-02-09 13:03:51 +0100

    first commit

---
+++ src/main/java/com/example/notaneitor/Sdi2122406SpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.notaneitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2122406SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2122406SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/notaneitor/Sdi2122406SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notaneitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2122406SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
