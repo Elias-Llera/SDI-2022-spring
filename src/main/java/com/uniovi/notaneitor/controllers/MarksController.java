a01a697a1101ce2794cef60c24953add435e9878
Elias <eliaslleragr@gmail.com>
2022-02-16 16:18:27 +0100

    SDI-2122-406-3.1-Thymeleaf, fragmentos.

--- src/main/java/com/uniovi/notaneitor/controllers/MarksController.java
+++ src/main/java/com/uniovi/notaneitor/controllers/MarksController.java
@@ -19,6 +19,12 @@ public class MarksController {
         return "mark/list";
     }
 
+    @RequestMapping("/mark/list/update")
+    public String updateList(Model model){
+        model.addAttribute("markList", marksService.getMarks() );
+        return "mark/list::tableMarks";
+    }
+
     @RequestMapping(value = "/mark/add")
     public String getMark() {
         return "mark/add";
